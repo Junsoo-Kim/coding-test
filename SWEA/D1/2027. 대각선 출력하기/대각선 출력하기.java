a5f1df987895522785df7deecd25cab1082a44b2
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-07 06:53:51 +0900

    [D1] Title: 대각선 출력하기, Time: 72 ms, Memory: 22,400 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+ 	public static void main(String[] args) throws Exception{
+        System.out.print("#++++\n+#+++\n++#++\n+++#+\n++++#");
+    }
+}

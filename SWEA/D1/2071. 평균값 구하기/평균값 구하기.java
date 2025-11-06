4366140f6819ef2cd3cd0aef298118e079b4f8a0
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-07 04:24:49 +0900

    [D1] Title: 평균값 구하기, Time: 89 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        sc.nextLine();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            String[] strList = sc.nextLine().split(" ");
+			double sum = 0;
+            
+            for(int i = 0; i < strList.length; i++){
+             	   sum += Integer.valueOf(strList[i]);
+            }
+            
+         	System.out.println("#" + test_case + " " + Math.round(sum / strList.length));
+		}
+	}
+}

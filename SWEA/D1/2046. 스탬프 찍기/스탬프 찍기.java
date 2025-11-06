b9ddd25e6e69a31438f77e86d73a833939a271cb
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-07 06:10:15 +0900

    [D1] Title: 스탬프 찍기, Time: 74 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+	public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < num; i++){
+            System.out.print("#");
+        }
+    }
+}

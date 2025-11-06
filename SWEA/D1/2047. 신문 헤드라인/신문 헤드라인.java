4394d3b9fe11f276a140c4f0a924ef6a769ced0f
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-07 06:14:04 +0900

    [D1] Title: 신문 헤드라인, Time: 74 ms, Memory: 23,808 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+	public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        str = str.toUpperCase();
+        
+        System.out.println(str);
+    }
+}

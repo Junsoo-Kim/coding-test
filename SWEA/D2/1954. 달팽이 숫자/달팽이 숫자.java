78e7512ed2f84cbe5dbfb919d112eb9cc55f867f
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-07 13:33:32 +0900

    [D2] Title: 달팽이 숫자, Time: 76 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -2,29 +2,30 @@ import java.util.*;
 import java.io.*;
 
 class Solution{
- 	public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        
+         
         for(int t = 1; t <= T; t++){
-         	int N = Integer.parseInt(br.readLine());
-            int size = N;
+            int N = Integer.parseInt(br.readLine());
+            int size = N; // N값이 로직 내에서 변하므로, 원본 크기 저장
             int[][] numList = new int[N][N];
-            
+             
             // 상 하 좌 우 좌표값의 한계값
             int currentNum = 1;
             int upperLimit = -1;
             int lowerLimit = N;
             int leftLimit = -1;
             int rightLimit = N;
-                
+                 
             // 우 방향으로 이동 N만큼 먼저 해야함
             for(int i = 0; i < rightLimit; i++){
                 numList[0][i] = currentNum;
                 currentNum++;
             }
             N--;
-            
+             
             while(true){
                 if(N == 0){break;}
                 // 하 방향으로 이동
@@ -33,7 +34,7 @@ class Solution{
                     numList[i][rightLimit - 1] = currentNum;
                     currentNum++;
                 }
-                
+                 
                 // 좌 방향으로 이동
                 rightLimit--;
                 for(int i = rightLimit - 1; i > leftLimit; i--){
@@ -41,7 +42,7 @@ class Solution{
                     currentNum++;
                 }
                 N--;
-                  
+                   
                 if(N == 0){break;}
                 // 상 방향으로 이동
                 lowerLimit--;
@@ -49,24 +50,26 @@ class Solution{
                     numList[i][leftLimit + 1] = currentNum;
                     currentNum++;
                 }
-                
+                 
                 // 우 방향으로 이동
                 leftLimit++;
                 for(int i = leftLimit + 1; i < rightLimit; i++){
                     numList[upperLimit + 1][i] = currentNum;
-                	currentNum++;
+                    currentNum++;
                 }  
-                
+                 
                 N--; 
             }
-            
-            System.out.println("#" + t);
+             
+            sb.append("#").append(t).append("\n");
             for(int i = 0; i < size; i++){
-             	for(int j = 0; j < size; j++){
-                    System.out.print(numList[i][j] + " ");
+                for(int j = 0; j < size; j++){
+                    sb.append(numList[i][j]).append(" ");
                 }
-                System.out.println();
+                sb.append("\n");
             }
         }
+        br.close();
+        System.out.print(sb); 
     }        
 }

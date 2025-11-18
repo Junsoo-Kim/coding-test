ff3f72101473ee2c508a804c692ecb2ba875a284
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-19 05:44:57 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 98 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,36 +1,42 @@
 import java.util.*;
 import java.io.*;
 
-class Solution{
- 	public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        for(int t = 1; t <= 10; t++){
-        	int N = Integer.parseInt(br.readLine());
-        	int maxIndex = 0;
-        	int minIndex = 0;
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	ArrayList<Integer> heightList = new ArrayList<>();
-        	
-        	for(int i = 0; i < 100; i++) {
-        		heightList.add(Integer.parseInt(st.nextToken()));
-        	}
-        	
-        	while(N > 0) {
-        		maxIndex = heightList.indexOf(Collections.max(heightList));
-        		minIndex = heightList.indexOf(Collections.min(heightList));
-        		
-        		if(maxIndex == minIndex) {break;}
-        		
-        		heightList.set(maxIndex, heightList.get(maxIndex) - 1);
-        		heightList.set(minIndex, heightList.get(minIndex) + 1);
-        		
-        		N--;
-        	}
-        	sb.append("#" + t + " ");
-        	sb.append((Collections.max(heightList) - Collections.min(heightList)) + "\n");
-        }
-        System.out.print(sb);
-    }
+class Solution {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		for (int tc = 1; tc <= 10; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int maxIndex = 0;
+			int minIndex = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			ArrayList<Integer> boxHeights = new ArrayList<>();
+
+			for (int i = 0; i < 100; i++) {
+				boxHeights.add(Integer.parseInt(st.nextToken()));
+			}
+
+			while (N > 0) {
+				// 최대값, 최소값 인덱스 탐색
+				maxIndex = boxHeights.indexOf(Collections.max(boxHeights));
+				minIndex = boxHeights.indexOf(Collections.min(boxHeights));
+
+				// 전부 평탄화 되었을 때 종료
+				if (maxIndex == minIndex) {
+					break;
+				}
+				
+				// 덤프 1회 실행
+				boxHeights.set(maxIndex, boxHeights.get(maxIndex) - 1);
+				boxHeights.set(minIndex, boxHeights.get(minIndex) + 1);
+
+				N--;
+			}
+			sb.append("#" + tc + " ");
+			sb.append((Collections.max(boxHeights) - Collections.min(boxHeights)) + "\n");
+		}
+		System.out.print(sb);
+	}
 }

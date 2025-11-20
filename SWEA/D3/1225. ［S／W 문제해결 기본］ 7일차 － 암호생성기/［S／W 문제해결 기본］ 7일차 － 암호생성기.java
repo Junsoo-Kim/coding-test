38135969acc771b3de7f4b664c5aa8d6c0ce3813
Junsoo Kim <kjsoo1901@gmail.com>
2025-11-21 00:10:28 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 121 ms, Memory: 29,952 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -6,34 +6,37 @@ public class Solution {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < 10; i++) {
-			int tc = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= 10; t++) {
+			int testNum = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			Queue<Integer> queue = new LinkedList<>();
+			int currentNum = 0;
 
-			for (int j = 0; j < 8; j++) {
+			for (int i = 0; i < 8; i++) {
 				queue.offer(Integer.parseInt(st.nextToken()));
 			}
 
-			int value = 1;
 			while (true) {
-				int num = queue.poll() - value;
-
-				if (num <= 0) {
-					queue.offer(0);
-					break;
+				for (int i = 1; i <= 5; i++) {
+					currentNum = queue.poll() - i;
+					// 현재 값이 0이면 루프 종료
+					if (currentNum <= 0) {
+						currentNum = 0;
+						queue.offer(currentNum);
+						break;
+					} else {
+						queue.offer(currentNum);
+					}
 				}
 
-				queue.offer(num);
-				value++;
-
-				if (value > 5)
-					value = 1;
+				if (currentNum == 0) {
+					break;
+				}
 			}
 
-			sb.append("#").append(tc);
-			for (int num : queue) {
-				sb.append(" ").append(num);
+			sb.append("#" + testNum + " ");
+			for (int i = 0; i < 8; i++) {
+				sb.append(queue.poll() + " ");
 			}
 			sb.append("\n");
 		}

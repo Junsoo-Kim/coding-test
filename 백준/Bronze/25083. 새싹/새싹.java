11db9eccc63c82626e24d75b42e30542c2506e9d
Junsoo Kim <kjsoo1901@gmail.com>
2026-01-14 12:54:47 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("         ,r\'\"7\n");
+		sb.append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n");
+		sb.append("   `~\\/\n");
+		sb.append("      |\n");
+		sb.append("      |\n");
+		
+		System.out.print(sb);
+	}
+}

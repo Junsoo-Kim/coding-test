e9ea99afeca782815ba73012d9c6df118e639619
Junsoo Kim <kjsoo1901@gmail.com>
2026-02-19 14:35:27 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N, M;
+    static int[] seq;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        seq = new int[M];
+        visited = new boolean[N];
+
+        bt(0);
+    }
+
+    static void bt(int depth){
+        if(depth == M){
+            StringBuilder sb = new StringBuilder();
+            for(int num : seq){
+                sb.append(num + 1).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(!visited[i]){
+                if(depth == 0){
+                    visited[i] = true;
+                    seq[depth] = i;
+
+                    bt(depth + 1);
+
+                    visited[i] = false;
+                }
+                else if(depth > 0 && seq[depth - 1] < i) {
+                    visited[i] = true;
+                    seq[depth] = i;
+
+                    bt(depth + 1);
+
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}

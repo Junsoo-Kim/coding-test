7c33d04f44468167194048dfaa648031232cc0b3
Junsoo Kim <kjsoo1901@gmail.com>
2026-04-08 23:50:26 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int i;
+    
+    if(a <= b){
+        for(i = a; i <= b; i++){
+            answer += i;
+        }
+    }
+    else{
+        for(i = b; i <= a; i++){
+            answer += i;
+        }
+    }
+
+    return answer;
+}

ed8423178fc13168d59389632c7775e0bd61f52a
Junsoo Kim <kjsoo1901@gmail.com>
2026-06-30 22:22:37 +0900

    [D1] Title: 중간값 찾기, Time: 76 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,21 +1,18 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        ArrayList<Integer> numList = new ArrayList<>();
-        
-        for(int test_case = 1; test_case <= T; test_case++){
-            numList.add(Integer.parseInt(st.nextToken()));
-        }
-        Collections.sort(numList);
-        
-        System.out.println(numList.get(T / 2));
+public class Solution {
+	public static void main(String args[]) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> nums = new ArrayList<>();
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Collections.sort(nums);
+		System.out.print(nums.get((nums.size() / 2))); // 결과값을 내림하기 위해 정수 타입으로 나눗셈
 	}
 }

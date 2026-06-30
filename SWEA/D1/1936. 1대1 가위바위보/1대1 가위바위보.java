a33c03fc26415cb6531f7b62cffa4742bc3a4ac3
Junsoo Kim <kjsoo1901@gmail.com>
2026-06-30 22:34:38 +0900

    [D1] Title: 1대1 가위바위보, Time: 73 ms, Memory: 22,272 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -1,18 +1,23 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution{
- 	public static void main(String args[]) throws Exception{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 	public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         
-        String result = (A > B) ? "A" : "B";
-        result = (A == 3 && B == 1) ? "B" : result;
-        result = (A == 1 && B == 3) ? "A" : result;
+        String result = (A < B) ? "B" : "A";
         
-        System.out.print(result);
-    }
+        if(A == 3 && B == 1) {
+        	result = "B";
+        }
+        else if(A == 1 && B == 3) {
+        	result = "A";
+        }
+        
+        System.out.print(result); 
+ 	}
 }

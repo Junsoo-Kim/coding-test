dc62677be92423b10008aeef191ba9d39095a92e
Junsoo Kim <kjsoo1901@gmail.com>
2026-07-02 19:13:05 +0900

    [D2] Title: 두 개의 숫자열, Time: 78 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -4,47 +4,50 @@ import java.io.*;
 class Solution{
  	public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         
         for(int t = 1; t <= T; t++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int result = -10001;
+            int result = Integer.MIN_VALUE;
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
+            int[] A = new int[N];
+            int[] B = new int[M];
             
-            // Ai와 Bj 선언 및 초기화
-            int[] aList = new int[N];
-            int[] bList = new int[M];
             st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < N; i++){
-                aList[i] = Integer.parseInt(st.nextToken());
-            }
-            st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < M; i++){
-                bList[i] = Integer.parseInt(st.nextToken());
+            for(int i = 0; i < N; i++) {
+            	A[i] = Integer.parseInt(st.nextToken());
             }
             
-            // 항상 N을 M보다 작게 해서 N과 aList를 단위로 사용
-           	if(N > M){
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < M; i++) {
+            	B[i] = Integer.parseInt(st.nextToken());
+            }
+            
+            // A의 크기를 더 작게 유지
+            if(N > M) {
             	int temp = N;
-                N = M;
-                M = temp;
-                
-                int[] tempList = aList;
-                aList = bList;
-                bList = tempList;
+            	N = M;
+            	M = temp;
+            	
+            	int[] tempArr = A;
+            	A = B;
+            	B = tempArr;
             }
             
-            // aList를 단위로 해서 bList를 한칸씩 이동시키며 합 구하기
-            for(int i = 0; i <= M - N; i++){
-                int currentSum = 0;
-             	for(int j = 0; j < N; j++){
-                	currentSum += aList[j] * bList[i + j]; 
-                }
-                result = (result < currentSum) ? currentSum : result;
+            for(int i = 0; i <= M - N; i++) {
+            	int sum = 0;
+            	
+            	for(int j = 0; j < N; j++) {
+            		sum += A[j] * B[i + j];
+            	}
+
+            	result = (result > sum) ? result : sum;
             }
             
-            System.out.println("#" + t + " " + result);
+            sb.append("#" + t + " " + result + "\n");
         }
+        System.out.print(sb);
     }
 }
